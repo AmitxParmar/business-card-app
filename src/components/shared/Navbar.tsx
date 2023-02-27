220417a67d48615691430e8b53dea8b15c530a16
AmitxParmar <amitparmar901@gmail.com>
2023-02-27 13:31:43 +0530

    setup index and home page and doing navbar

---
+++ src/components/shared/Navbar.tsx
@@ -0,0 +1,21 @@
+import { FC, Fragment } from 'react'
+import { Disclosure, Menu, Transition } from '@headlessui/react'
+import { HIBookOpen, HiX } from 'react-icons/hi'
+
+interface NavbarProps { }
+
+function classNames(...classes: string[]) {
+    return classes.filter(Boolean).join(' ')
+}
+
+import React from 'react'
+
+type Props = {}
+
+const Navbar = (props: Props) => {
+    return (
+        <div>Navbar</div>
+    )
+}
+
+export default Navbar
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,12 +4,12 @@ import Link from "next/link";
 import { signIn, signOut, useSession } from "next-auth/react";
 
 import { api } from "~/utils/api";
+import { useRouter } from "next/router";
 
 const Home: NextPage = () => {
   const { data: sessionData } = useSession();
 
-  const router = useRouter();
-
+  const router = useRouter()
 
   return (
     <>
@@ -20,7 +20,13 @@ const Home: NextPage = () => {
       </Head>
 
       <div className="flex h-[calc(100vh-4rem)] flex-col items-center justify-center bg-gradient-to-br from-rose-500 to-purple-600">
-        Test BG
+        {!sessionData && (
+          <button
+            onClick={sessionData ? () => signOut() : () => signIn('google')}
+            className='rounded-full bg-black/10 px-10 py-3 font-semibold text-black no-underline transition hover:bg-black/20'>
+            Sign in with Google
+          </button>
+        )}
       </div>
 
     </>
