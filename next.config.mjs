9fa2a67a095e57bed90996e86795f20a436a8ea3
AmitxParmar <amitparmar901@gmail.com>
2023-02-27 21:19:57 +0530

    fix: Next-images issue

--- next.config.mjs
+++ next.config.mjs
@@ -9,7 +9,20 @@
 /** @type {import("next").NextConfig} */
 const config = {
   reactStrictMode: true,
-
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'tailwindui.com',
+        port: '',
+        pathname: '/img/**',
+      },
+      {
+        protocol: "http",
+        hostname: "localhost",
+      },
+    ],
+  },
   /**
    * If you have the "experimental: { appDir: true }" setting enabled, then you
    * must comment the below `i18n` config out.
