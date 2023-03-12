df90c4e10ca3b9294cf9dd176de3c158854ffb57
AmitxParmar <amitparmar901@gmail.com>
2023-03-12 18:25:40 +0530

    style: BusinessCard: complete

--- src/components/BusinessCard/BusinessCard.tsx
+++ src/components/BusinessCard/BusinessCard.tsx
@@ -1,7 +1,7 @@
 import { BusinessCard } from '@prisma/client'
 import { useSession } from 'next-auth/react'
 import { type FC } from 'react'
-
+import Image from 'next/image'
 import React from 'react'
 
 type BusinessCardProps = {
@@ -13,10 +13,69 @@ type BusinessCardProps = {
 }
 
 const BusinessCard: FC<BusinessCardProps> = ({ inputs, card }) => {
+    const { data: sessionData } = useSession();
+
+    /* API end points */
+
+
+
     return (
         <div className='card'>
             <div className='card-back'>
-
+                <div className='line-numbers'>
+                    <div>1</div>
+                    <div>2</div>
+                    <div>3</div>
+                    <div>4</div>
+                    <div>5</div>
+                    <div>6</div>
+                    <div>7</div>
+                    <div>8</div>
+                    <div>9</div>
+                </div>
+                <code>
+                    <span className='variable'>const </span>
+                    <span className='function'>aboutMe </span>
+                    <span className='operator'>= </span>
+                    <span>{'{'} </span>
+                    <div className='indent'>
+                        {' '}
+                        <span className='property'>name</span>
+                        <span className='operator'>: </span>
+                        <span className='string'>&apos;{card ? card.name : sessionData?.user?.name}&apos;</span>
+                        <span>,</span>
+                    </div>
+                    <div className='indent'>
+                        {' '}
+                        <span className="property">title</span>
+                        <span className="operator">: </span>
+                        <span className="string">&apos;{card ? card.title : inputs?.title}&apos;</span>
+                        <span>,</span>
+                    </div>
+                    <div className="indent">
+                        {' '}
+                        <span className='property'>contact</span>
+                        <span className='operator'>: </span>
+                        <span>{'{'}</span>
+                        <div className='indent'>
+                            {' '}
+                            <span className='property'>email</span>
+                            <span className='operator'>: </span>
+                            <span className='string'>&apos;{card ? card.email : sessionData?.user?.email}&apos;</span>
+                            <span>,</span>
+                        </div>
+                        <div className='indent'>
+                            <span className='property'>website</span>
+                            <span className='operator'>:</span>
+                            <span className='string'>&apos;{card ? card.website : inputs?.website}&apos;</span>
+                        </div>
+                        <span>{'}'}</span>
+                    </div>
+                    <span>{'}'}</span>
+                </code>
+            </div>
+            <div className='card-front'>
+                <Image height={480} width={480} className='h-[30rem] w-[30rem]' src={front} alt={"card front"} />
             </div>
         </div>
     )
